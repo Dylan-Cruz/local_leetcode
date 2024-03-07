96c157db9f88607bbad9a4ae1f55b93efceafaee
Dylan Cruz <9627014+Dylan-Cruz@users.noreply.github.com>
2024-03-07 13:38:46 -0500

    wip switch to selenium

---
+++ src/leetcode_problem.py
@@ -0,0 +1,12 @@
+from typing import Dict
+
+
+class LeetcodeProblem:
+    def __init__(self, driver: WebDriver, url):
+        self.driver = driver
+
+    def load(self) -> None:
+        driver.
+
+    def scrape_content(self) -> Dict:
+        return {}
